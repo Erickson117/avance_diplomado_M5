dec513bce34981844f253f255fceb18c3855348c
Erickson117 <erickreyes07@aragon.unam.mx>
2024-09-29 13:53:54 -0600

    avance

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-Project_1_prueba_v1-0.1";
+const contentToCache = [
+    "Build/Avance_ParticlesShaders.loader.js",
+    "Build/Avance_ParticlesShaders.framework.js",
+    "Build/Avance_ParticlesShaders.data",
+    "Build/Avance_ParticlesShaders.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
